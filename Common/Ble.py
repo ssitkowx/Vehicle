1af2597adaed7ab7280af5b6e92c7f46e4d7c7f2
Sylwester <sitkowskis@interia.pl>
2022-06-04 17:54:29 +0200

    update

--- Common/Ble.py
+++ Common/Ble.py
@@ -3,8 +3,9 @@ from Logger import *
 
 class Ble:
     def __init__ (self):
+         os.system ('sudo hciconfig hci0 piscan')
+
+    def __del__ (self):
          os.system ('sudo systemctl restart bluetooth')
          os.system ('sudo hcitool dev')
          os.system ('sudo sdptool add SP')
-         os.system ('sudo hciconfig hci0 piscan')
-
