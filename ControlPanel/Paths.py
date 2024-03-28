8c36e75ddbe997e26d11fe6a88b5c25658541251
Sylwester <sitkowskis@interia.pl>
2024-03-28 15:01:35 +0100

    restor

--- ControlPanel/Paths.py
+++ ControlPanel/Paths.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-sys.path.append ('../Common')
-sys.path.append ('../ControlPanel')
-sys.path.append ('../ControlPanel/Hardware')
-sys.path.append ('../ControlPanel/Hardware/Peripheral')
-sys.path.append ('../ControlPanel/Logic')
-sys.path.append ('../ControlPanel/Logic/Gui')
-sys.path.append ('../ControlPanel/Logic/Gui/Panels')
-sys.path.append ('../ControlPanel/Logic/Gui/Widgets')
+sys.path.append ('Common')
+sys.path.append ('ControlPanel')
+sys.path.append ('ControlPanel/Hardware')
+sys.path.append ('ControlPanel/Hardware/Peripheral')
+sys.path.append ('ControlPanel/Logic')
+sys.path.append ('ControlPanel/Logic/Gui')
+sys.path.append ('ControlPanel/Logic/Gui/Panels')
+sys.path.append ('ControlPanel/Logic/Gui/Widgets')
