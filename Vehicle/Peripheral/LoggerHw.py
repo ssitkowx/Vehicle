169cd5569eda2d6c5efab3759c0a97980b405470
Demo User <sitkowskis@interia.pl>
2024-03-30 07:48:12 +0000

    Logger update

--- Vehicle/Peripheral/LoggerHw.py
+++ Vehicle/Peripheral/LoggerHw.py
@@ -4,6 +4,6 @@ class LoggerHw (Logger):
     def __init__ (self):
         pass
     
-    def log (self, vLogLevel: str, vMsg: str):
-        msg = super ().log (vLogLevel, vMsg)
+    def log (self, vLogLevel: str, vModule: str, vMsg: str):
+        msg = super ().log (vLogLevel, vModule, vMsg)
         print (msg)
