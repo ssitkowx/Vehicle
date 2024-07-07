1bafc88d22034c29ee5d5a57e6afbfee7e64743c
sylwester.sitkowski <sylwester.sitkowski@gunnebo.com>
2024-07-07 21:19:06 +0200

    Timer added

--- ControlPanel/Logic/Gui/Widgets/Labels (copy).py
+++
@@ -1,7 +0,0 @@
-from PySide6.QtWidgets import QLabel
-
-class Labels:
-    def __init__ (self):
-        self.roll  = QLabel ("Roll: ")
-        self.pitch = QLabel ("Pitch: ")
-        self.yaw   = QLabel ("Yaw: ")
